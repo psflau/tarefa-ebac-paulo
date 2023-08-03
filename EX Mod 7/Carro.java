f88ba7edbfe049df6c3a18351d7ae5168dcda331
Paulo Flau <santosflau@gmail.com>
2023-08-03 12:03:48 -0300

    Exercicio do modulo 7

---
+++ EX Mod 7/Carro.java
@@ -0,0 +1,51 @@
+package aula32;
+
+public class Carro {
+
+	private String marca;
+	private String modelo;
+	private int numPassageiro;
+	private double capCombustivel;
+	private double consumoCombustivel;
+	
+	public String getMarca() {
+		return this.marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getNumPassageiro() {
+		return numPassageiro;
+	}
+
+	public void setNumPassageiro(int numPassageiro) {
+		this.numPassageiro = numPassageiro;
+	}
+
+	public double getCapCombustivel() {
+		return capCombustivel;
+	}
+
+	public void setCapCombustivel(double capCombustivel) {
+		this.capCombustivel = capCombustivel;
+	}
+
+	public double getConsumoCombustivel() {
+		return consumoCombustivel;
+	}
+
+	public void setConsumoCombustivel(double consumoCombustivel) {
+		this.consumoCombustivel = consumoCombustivel;
+	}
+	
+}
---
+++ EX Mod 7/TesteCarro.java
@@ -0,0 +1,13 @@
+package aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		Carro van = new Carro();
+		van.setMarca("mercedes");
+		String nome =van.getMarca();
+		System.out.println(nome);
+
+	}
+
+}
