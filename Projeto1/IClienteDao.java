6cb8eb0d40f42569a0e6388d80aaecdb82b0e329
pauloflau <pauloflau@gmail.com>
2023-11-11 21:13:37 -0300

    Envio do projeto1

---
+++ Projeto1/IClienteDao.java
@@ -0,0 +1,15 @@
+package Projeto1;
+
+import java.util.Collection;
+
+public interface IClienteDao {
+	public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
