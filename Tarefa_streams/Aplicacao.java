aff887c9c216826b40ad3bd330a00b4c4c4ea36e
pauloflau <pauloflau@gmail.com>
2024-02-26 09:48:54 -0300

    envio do exercicio do modulo de streams

---
+++ Tarefa_streams/Aplicacao.java
@@ -0,0 +1,35 @@
+package cap22;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+
+        System.out.println("Por favor digite o nome e o gênero, separados por traço e vírgula.");
+        System.out.println("Exemplo:\n" + "MARCIO-MASCULINO,LUARA-FEMININO,LETICIA-FEMININO\n");
+
+        System.out.println("Digite aqui --> ");
+        String entrada = scanner.nextLine();
+        entrada = entrada.toLowerCase();
+        String[] pessoas = entrada.split(",");
+
+        for (String pessoa : pessoas) {
+            lista.add(pessoa);
+        }
+
+        List<String> listaMulheres = lista.stream()
+                .filter(p -> p.split("-")[1].equals("feminino"))
+                .toList();
+
+        System.out.println("Lista das Mulheres: ");
+        listaMulheres.forEach(System.out::println);
+        scanner.close();
+
+	}
+
+}
