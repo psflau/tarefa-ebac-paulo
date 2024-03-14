1a8cae9df5790be1ebb5bcec00ce30505d697703
pauloflau <pauloflau@gmail.com>
2024-03-14 12:19:28 -0300

    Tarefa modulo 32

---
+++ produto2/src/main/java/com/api/produto/modelo/Produto.java
@@ -0,0 +1,45 @@
+package com.api.produto.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="Codigo")
+	private int codigo;
+	
+	@Column(name="Nome")
+	private String nome;
+	
+	@Column(name="VALOR")
+	private double valor;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
